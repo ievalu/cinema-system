6880d54d4f1ca856e110858c8d1b17a4b3a41c80
ieva.lubyte <Balionai666+>
2019-03-19 08:40:39 +0200

    Add new director test

---
+++ test/modules/director/DirectorRepositorySpec.scala
@@ -0,0 +1,57 @@
+package modules.director
+
+import java.sql.Date
+
+import com.dimafeng.testcontainers.PostgreSQLContainer
+import modules.util.{Country, Gender}
+import org.scalatest.TestData
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.Application
+import utility.application.TestApplications.basicDatabaseTestApplication
+import utility.database.PlayPostgreSQLTest
+
+import scala.concurrent.ExecutionContext
+
+class DirectorRepositorySpec extends PlaySpec
+  with GuiceOneAppPerTest
+  with PlayPostgreSQLTest
+  with ScalaFutures
+  with IntegrationPatience {
+
+  override val container: PostgreSQLContainer = PostgreSQLContainer("postgres:alpine")
+
+  override def newAppForTest(testData: TestData): Application = {
+    basicDatabaseTestApplication(container, evolutionsEnabled = false)
+  }
+
+  "Director repository" must {
+    "create new director" in {
+      implicit lazy val executionContext = app.injector.instanceOf[ExecutionContext]
+      withEvolutions { () =>
+        val repo = app.injector.instanceOf[DirectorRepository]
+        val director = repo
+          .create(new CreateDirectorForm(
+            "name",
+            "lastName",
+            new Date(10000),
+            Country.USA,
+            187,
+            Gender.Male
+            )
+          ).futureValue
+
+        director mustEqual Director(
+          3,
+          "name",
+          "lastName",
+          new Date(10000),
+          Country.USA,
+          187,
+          Gender.Male
+        )
+      }
+    }
+  }
+}
